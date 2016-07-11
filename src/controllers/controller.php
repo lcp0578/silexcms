4ab92e1611bd5d59a5af6e2c61090cb5e18d2fef
李春鹏 <lcp0578@gmail.com>
2016-07-11 23:55:49 +0800

    [ADD] add bootstrap and controller

---
+++ src/controllers/controller.php
@@ -0,0 +1,10 @@
+<?php
+/**
+  * filename
+  *
+  * @package: packname
+  * @author: lcp0578@gmail.com
+  * @date: 2016-07-11 11:45:37
+  * @version: 0.0.1
+  * @copyright: http://lcpeng.cn
+  */
---
+++ src/core/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+/**
+  * filename
+  *
+  * @package: packname
+  * @author: lcp0578@gmail.com
+  * @date: 2016-07-11 11:48:08
+  * @version: 0.0.1
+  * @copyright: http://lcpeng.cn
+  */
+require_once __DIR__.'/../../vendor/autoload.php';
