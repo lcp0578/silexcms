ffd1a482d9c2f4c663077c5495c5b4fb359e514f
李春鹏 <lcp0578@gmail.com>
2016-07-22 17:03:09 +0800

    [BUG] email bug

--- src/controller/Controller.php
+++ src/controller/Controller.php
@@ -10,7 +10,7 @@
  */
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Validator\Constraint as Assert;
+use Symfony\Component\Validator\Constraints as Assert;
 
 $app->get('/', function () use($app)
 {
